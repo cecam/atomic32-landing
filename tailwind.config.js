bce1273aee711bf60327d0ea2ba93db96c3b7041
Celso Cardenas <ccardena@tibco.com>
2023-04-02 16:03:14 -0600

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'float-element': 'float 5s ease-in-out infinite'
+      },
+      keyframes: {
+        float: {
+          '0%': { transform: 'translateY(0%)'},
+          '50%': { transform: 'translateY(3%)'},
+          '100%': { transform: 'translateY(0%)'},
+        }
+      }
+    },
   },
   plugins: [],
 }
